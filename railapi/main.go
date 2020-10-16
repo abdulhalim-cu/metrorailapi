f353b8787b5e82778ad49dc3124b88847b93733c
abdulhalim <abdul.halim@bs-23.net>
2020-10-16 11:07:35 +0600

    [WIP] Global DB var and Train, Station, and Schedule struct added
    DB is a global variable declared to make it visible to whole program.
    
    TrainResouce -> struct to hold the information about trains
    StationResource -> struct to hold the information about stations
    ScheduleResource -> struct to link the trains and stations. i.e., it holds when and where a trian will arrive.

--- railapi/main.go
+++ railapi/main.go
@@ -3,10 +3,37 @@ package main
 import (
 	"database/sql"
 	"log"
+	"time"
 
 	"github.com/abdulhalim-cu/metrorailapi/dbutils"
 	_ "github.com/mattn/go-sqlite3"
 )
+
+// DB driver visible to whole program
+var DB *sql.DB
+
+// TrainResource is the model for holding rail information
+type TrainResource struct {
+	ID              int
+	DriverName      string
+	OperatingStatus bool
+}
+
+// StationResource holds the information about locations
+type StationResource struct {
+	ID          int
+	Name        string
+	OpeningTime time.Time
+	ClosingTime time.Time
+}
+
+// ScheduleResource links both trains and stations
+type ScheduleResource struct {
+	ID          int
+	TrainID     int
+	StationID   int
+	ArrivalTime time.Time
+}
 
 func main() {
 
