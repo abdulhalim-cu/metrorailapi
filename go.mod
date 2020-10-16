d473b603162947131f071f0393616d9f79490e2a
abdulhalim <abdul.halim@bs-23.net>
2020-10-16 12:43:49 +0600

    Add Regsiter() to register webservices and routes the path to GET, POST, and DELETE
    Create a new WebService and add paths to it. A path is a URL endpoint and
    routes are the path parameters or query parameters attached to the function
    handlers. A webservice created to serve the Train resource. Three REST method
    attached to three function handlers, getTrain, createTrain, and deleteTrain respectively
    All these REST methods are defined on the instance of the TrainResource struct.

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/abdulhalim-cu/metrorailapi
 
 go 1.15
 
-require github.com/mattn/go-sqlite3 v1.14.4
+require (
+	github.com/emicklei/go-restful v2.14.2+incompatible
+	github.com/mattn/go-sqlite3 v1.14.4
+)
--- railapi/main.go
+++ railapi/main.go
@@ -2,10 +2,13 @@ package main
 
 import (
 	"database/sql"
+	"encoding/json"
 	"log"
+	"net/http"
 	"time"
 
 	"github.com/abdulhalim-cu/metrorailapi/dbutils"
+	"github.com/emicklei/go-restful"
 	_ "github.com/mattn/go-sqlite3"
 )
 
@@ -33,6 +36,66 @@ type ScheduleResource struct {
 	TrainID     int
 	StationID   int
 	ArrivalTime time.Time
+}
+
+// Register adds path and routes to container
+func (t *TrainResource) Register(container *restful.Container) {
+	ws := new(restful.WebService)
+	ws.
+		Path("/v1/trains").
+		Consumes(restful.MIME_JSON).
+		Produces(restful.MIME_JSON) // we can specify this per route as well.
+
+	ws.Route(ws.GET("/{train-id}").To(t.getTrain))
+	ws.Route(ws.POST("").To(t.createTrain))
+	ws.Route(ws.DELETE("/{train-id}").To(t.deleteTrain))
+	container.Add(ws)
+}
+
+// GET http://base-url/v1/trains/1
+func (t TrainResource) getTrain(request *restful.Request, response *restful.Response) {
+	id := request.PathParameter("train-id")
+	err := DB.QueryRow("SELECT ID, DRIVER_NAME, OPERATING_STATUS FROM train WHERE id=?", id).Scan(&t.ID, &t.DriverName, &t.OperatingStatus)
+	if err != nil {
+		log.Println(err)
+		response.AddHeader("Content-Type", "text/plain")
+		response.WriteErrorString(http.StatusNotFound, "Train couldn't be found.")
+	} else {
+		response.WriteEntity(t)
+	}
+}
+
+// POST http://base-url/v1/trains
+func (t TrainResource) createTrain(request *restful.Request, response *restful.Response) {
+	log.Println(request.Request.Body)
+	decoder := json.NewDecoder(request.Request.Body)
+	var b TrainResource
+	err := decoder.Decode(&b)
+	log.Println(b.DriverName, b.OperatingStatus)
+	// Error handling is obvious here. So omitting..
+	stmt, _ := DB.Prepare("INSERT INTO train (DRIVER_NAME, OPERATING_STATUS) VALUES (?, ?)")
+	result, err := stmt.Exec(b.DriverName, b.OperatingStatus)
+	if err == nil {
+		newID, _ := result.LastInsertId()
+		b.ID = int(newID)
+		response.WriteHeaderAndEntity(http.StatusCreated, b)
+	} else {
+		response.AddHeader("Content-Type", "text/plain")
+		response.WriteErrorString(http.StatusInternalServerError, err.Error())
+	}
+}
+
+// DELETE http://base-url/v1/trains/1
+func (t TrainResource) deleteTrain(request *restful.Request, response *restful.Response) {
+	id := request.PathParameter("train-id")
+	stmt, _ := DB.Prepare("DELETE FROM train WHERE id=?")
+	_, err := stmt.Exec(id)
+	if err == nil {
+		response.WriteHeader(http.StatusOK)
+	} else {
+		response.AddHeader("Content-Type", "text/plain")
+		response.WriteErrorString(http.StatusInternalServerError, err.Error())
+	}
 }
 
 func main() {
